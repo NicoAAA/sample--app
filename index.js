fdac9d6c18be26906f683940ea489e28b6cad3cf
NicoAAA <nicolas.acosta@soy.sena.edu.co>
2025-09-08 17:23:26 -0500

    fix: Bind Express server to 0.0.0.0

--- index.js
+++ index.js
@@ -76,4 +76,6 @@ app.get('/dashboard', (req, res) => {
     `);
 });
 
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor escuchando en http://0.0.0.0:${PORT}`);
+});
