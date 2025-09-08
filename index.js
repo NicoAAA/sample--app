322612f0ca82b2bac55b8029c90278d767e5536c
NicoAAA <nicolas.acosta@soy.sena.edu.co>
2025-09-08 15:03:47 -0500

    Update index.js Ruta de Login en minusculas login

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const PORT = 5050;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/Login', (req, res) => {
+app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
-app.post('/Login', (req, res) => {
+app.post('/login', (req, res) => {
     const { username, password } = req.body;
     if (username === 'admin' && password === 'admin123'){
         return res.redirect('/dashboard');
